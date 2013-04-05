42764445da5aa3b07c713a1481e8a8b962ba0221
Vinicius Vendramini <vinivendra@gmail.com>
2013-04-05 18:42:25 -0300

    Commit quase do final do ep; falta organizar o codigo e checar os ultimos bugs possiveis

--- ED1/Pontos.h
+++ ED1/Pontos.h
@@ -2,16 +2,32 @@
 #include <stdlib.h>
 #include <math.h>
 
-typedef struct {
+typedef struct point{
     float x;
     float y;
+    int link;
+    int n;
 } point;
 
-typedef struct {
+typedef struct cell{
+    point *point;
+    struct cell *prox; /*proximo no grid*/
+    struct cell *next; /*proximo na lista de (des)conectados*/
+    struct cell *prev; /*anterior na lista de (des)conectados*/
+} cell;
+
+typedef struct distance{
     point *p1;
     point *p2;
     float value;
 } distance;
+
+typedef struct aresta{
+    cell *c1;
+    cell *c2;
+    float d;
+    struct aresta *prox;
+} aresta;
 
 point *initPoint ();
 
@@ -27,9 +43,10 @@ distance *initDistanceBetween (point *p1, point *p2);
 
 float distanceBetween (point *p1, point *p2);
 
+float squaredDistanceBetween (point *p1, point *p2);
+
 float square (float value);
 
 int distanceHasPoint (distance *distance, point *p1);
 
-
-
+void insertPointInCell (point *point, cell *celula);
